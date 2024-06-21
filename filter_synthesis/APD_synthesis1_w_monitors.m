b1951c2a8f50d40fb2d2684203dc335dc2bb1bc4
AMPIC <vsaxena@udel.edu>
2024-06-21 03:59:05 -0400

    Added Matlab APD Synthesis code

--- filter_synthesis/APD_synthesis1_w_monitors.m
+++ filter_synthesis/APD_synthesis1_w_monitors.m
@@ -7,6 +7,8 @@
 % Optical Filters with Optimum Bandpass Designs," IEEE Photonics Technology
 % Letters, vol. 10, no. 8, Aug 1998
 %
+% Modifications for Dual-Bus Rings with Monitor
+%
 % (c) Vishal Saxena 2015
 % Last modified: June/21/2024
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -14,12 +16,12 @@
 clear all; close all; clc;
 
 % Generate Chebyshev II IIR Filter Polynomial
-order = 4;
+order = 2;
 Rs = 40; %  dB of stopband attenuation down from the peak passband value 
 %Wn = [3e9 4e9]/20e9;% stopband edge frequency
 Wn = 0.13;% stopband edge frequency
-[z,p,k] = cheby2(order, Rs, Wn, 'low');
-%[z,p,k] = butter(order,Wn,'low');
+%[z,p,k] = cheby2(order, Rs, Wn, 'low');
+[z,p,k] = butter(order,Wn,'low');
 % sos = zp2sos(z,p,k);
 % fvtool(sos,'Analysis','freq')
 
@@ -32,8 +34,8 @@ G = filt(b,a)
 %zplane(b,a)
 
 figure()
-plot(w*20/pi,mag2db(abs(h)), 'r', 'lineWidth', 2)
-axis([-20 20 -70 5])
+plot(w*20/pi,mag2db(abs(h)),'lineWidth', 2)
+%axis([-20 20 -70 5])
 grid
 title('Optical Filter Response');
 xlabel('frequency (GHz)')
@@ -86,6 +88,8 @@ phi2 = angle(zn2);     % Phase shifts in the rings
 % Top Arm rings
 Htop = filt([1], [1], 1);
 
+figure();
+hold on; grid;
 
 for j = 1:ord1
     ejphi = exp(i*phi1(j));
@@ -111,11 +115,47 @@ for j = 1:ord1
     hr = freqz(nn, dd, ww);
     hm = freqz(nn2, dd2, ww);
 
-    %plot(ww*20/pi,mag2db(abs(hr)), 'b', 'lineWidth', 1)
-    %plot(ww*20/pi,mag2db(abs(hm)), 'lineWidth', 1.5)
+   % plot(ww*20/pi,mag2db(abs(hr)), 'b', 'lineWidth', 1)
+    plot(ww*20/pi,mag2db(abs(hm)), 'lineWidth', 1.5)
 end    
 
+title('Optical Monitor Responses');
+xlabel('frequency (GHz)')
+ylabel('Attenuation (dB)')
+
+% Bottom Arm rings
+
+Hbot = filt([1], [1], 1);
+
+for j = 1:ord2
+    ejphi = exp(i*phi2(j));
+    t = t2(j);
+    sr = sqrt(k2(j));
+    sm = sqrt(km);
+    cr = sqrt(1-k2(j));
+    cm = sqrt(1-km);
+    %ejphi = exp(i*phi);
+
+    bb = [0 -sr*sm*r*ejphi];
+    aa = [1 -cr*cm*r*ejphi];
+    Hr =  filt(bb, aa, 1);
+
+    bb1 = [cr -cm*r*ejphi];
+    Hm = filt(bb1, aa, 1);
+    
+    [nn, dd] = tfdata(Hr,'v');
+    [nn2, dd2] = tfdata(Hm,'v');
+    ww = linspace(-pi, pi, 2^12);
+    hr = freqz(nn, dd, ww);
+    hm = freqz(nn2, dd2, ww);
+
+   % plot(ww*20/pi,mag2db(abs(hr)), 'b', 'lineWidth', 1)
+    plot(ww*20/pi,mag2db(abs(hm)), 'lineWidth', 1.5)
+    Hbot = Hbot*Hm;
+end
+
 % Plot Result Summary
+
 
 fprintf('Top Arm Rings:\n');
 fprintf('----------------------------\n');
@@ -144,5 +184,47 @@ disp('Filter beta - phi_tot');
 disp(beta-phi_tot);
 
 
+%% Complete Filter with couplers and phase shifter
+% Tap at the bar output (Top-Bot)
+GG = 0.5*(Htop*exp(i*(beta-phi_tot)) + Hbot*exp(i*(-beta+phi_tot)));
+%GG = 0.5*(Htop*exp(i*(beta)) + Hbot*exp(i*(-beta)));
+%GG = 0.5*j*(Htop + Hbot);
+%GG = Htop;
+[nn4, dd4] = tfdata(GG,'v');
+[hh,ww] = freqz(nn4, dd4,2^14);
 
+% figure()
+% plot(ww/pi,mag2db(abs(hh)), 'r', 'lineWidth', 2)
+% %plot(ww/pi,angle(hh), 'r', 'lineWidth', 2)
+% %axis([0 1 -50 5])
+% grid
+% title('Cascade Ring Filter');
+% xlabel('Normalized Frequency (\omega / \pi)')
+% ylabel('Attenuation (dB)')
+
+
+
+figure()
+
+w = linspace(-pi, pi, 2^12);
+
+[b, a] = tfdata(G,'v');
+h1 = freqz(b,a,w);
+figure()
+zplane(b,a)
+
+[b, a] = tfdata(GG,'v');
+h2 = freqz(b,a,w);
+figure()
+zplane(b,a)
+
+plot(w*20/pi,mag2db(abs(h1).^1), 'b-.', 'lineWidth', 2); hold on;
+plot(w*20/pi,mag2db(abs(h2).^1), 'r', 'lineWidth', 2)
+axis([-20 20 -70 5])
+grid
+% set(gca,'FontSize',12);
+title('Optical Filter Response');
+xlabel('frequency (GHz)')
+ylabel('Attenuation (dB)')
+legend('Ideal', 'Si-photonic');
 
